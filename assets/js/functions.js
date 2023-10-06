8c33d3b974c37bcc7a3c68205232adc0c5657506
Stuart Russell <stuart.russell@protonmail.com>
2023-10-06 10:06:22 +0100

    fix greeting message

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,12 +4,11 @@ function setGreeting() {
     var today = new Date().getHours();
     if (today < 12) {
         greeting.textContent = "Madiann Math"
-    }
-    if (today > 12 && today < 13) {
+    } else if (today > 12 && today < 13) {
         greeting.textContent = "Feasgar Math"
-    }
-    if (today > 13 && today < 18) {
+    } else if (today > 13 && today < 18) {
         greeting.textContent = "Latha Math"
+    } else {
+        greeting.textContent = "Oidhche Math"
     }
-    greeting.textContent = "Oidhche Math"
 }
