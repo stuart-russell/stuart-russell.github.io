a73204bfcb8e3f5e5b387592864141e7d80f9d91
Stuart Russell <stuart.russell@protonmail.com>
2023-10-10 09:41:34 +0100

    spelling fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,12 +3,12 @@ function setGreeting() {
 
     var today = new Date().getHours();
     if (today < 12) {
-        greeting.textContent = "Madiann Math"
+        greeting.textContent = "Madiann Mhath"
     } else if (today > 12 && today < 13) {
         greeting.textContent = "Feasgar Math"
     } else if (today > 13 && today < 18) {
         greeting.textContent = "Latha Math"
     } else {
-        greeting.textContent = "Oidhche Math"
+        greeting.textContent = "Oidhche Mhath"
     }
 }
